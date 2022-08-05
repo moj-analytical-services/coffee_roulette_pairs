72900e27d0a7a835c79a10bf08718e3470be9f77
marco.surace@justice.gov.uk <marco.surace@justice.gov.uk>
2022-08-05 16:46:54 +0000

    updated lockfile

--- tests/testthat/test-generate_random_pairs.R
+++ tests/testthat/test-generate_random_pairs.R
@@ -1,3 +1,7 @@
+names <- c('Jack', 'Matt', 'George', 'Simon', 'Craig', 'Fred')
+names_wrong <- data.frame(P1=c('Jack', 'Matt', 'George'), P2=c('Simon', 'Craig', 'Fred'))
+uwp <- data.frame(P1='Jack', P2='Matt')
+
 test_that("generate_random_pairs", {
   expect_equal(2 * 2, 4)
 })
